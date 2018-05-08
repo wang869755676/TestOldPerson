58c681ec2b9bdeb74f700b0cc9c6d28dd68ede06
shanshan12345 <3049573633@qq.com>
2018-05-08 11:24:34 +0800

    java基本知识的测试

---
+++ app/src/main/java/com/jin/testoldperson/java/TestJava.java
@@ -0,0 +1,23 @@
+package com.jin.testoldperson.java;
+
+/**
+ * Created by my on 2018/5/8.
+ */
+
+public class TestJava {
+    public static void main(String[] args) {
+        TestJava java = new TestJava();
+        java.testBasicType();
+    }
+
+
+    public void testBasicType() {
+        System.out.println(3 * 0.1f);
+        System.out.println(4 * 0.1f);
+        System.out.println(5 * 0.1f);
+        System.out.println(6 * 0.1f);
+        System.out.println(0.3);
+
+
+    }
+}
